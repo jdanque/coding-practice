bdf285090a0f07bb1e513b55634c60937339cb4a
Jobo Danque <jmdanque@jmdanque.smi.com.ph>
2019-06-06 18:40:28 +0800

    refactor: move code duplicate to helper function

--- cracking-the-coding-interview/src/test/java/arrays_and_strings/RotateMatrixTest.java
+++ cracking-the-coding-interview/src/test/java/arrays_and_strings/RotateMatrixTest.java
@@ -3,8 +3,7 @@ package arrays_and_strings;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertArrayEquals;
 
 public class RotateMatrixTest {
     private RotateMatrix underTest;
@@ -25,28 +24,19 @@ public class RotateMatrixTest {
         x = new int[][]{{1, 2}, {3, 4}};
         expected = new int[][]{{3, 1}, {4, 2}};
         x = underTest.rotateRight(x);
-        for (int i = 0; i < expected.length; i++) {
-            int[] expect = expected[i];
-            assertArrayEquals(expect, x[i]);
-        }
+        check2dArray(x, expected);
 
         //3x3
         x = new int[][]{{1, 2, 3}, {4,5,6}, {7,8,9}};
         expected = new int[][]{{7,4,1}, {8,5,2}, {9,6,3}};
         x = underTest.rotateRight(x);
-        for (int i = 0; i < expected.length; i++) {
-            int[] expect = expected[i];
-            assertArrayEquals(expect, x[i]);
-        }
+        check2dArray(x, expected);
 
         //4x4
         x = new int[][]{{1, 2, 3, 4}, {5,6,7,8}, {9,10,11,12},{13,14,15,16}};
         expected = new int[][]{{13,9,5,1}, {14,10,6,2}, {15,11,7,3}, {16,12,8,4}};
         x = underTest.rotateRight(x);
-        for (int i = 0; i < expected.length; i++) {
-            int[] expect = expected[i];
-            assertArrayEquals(expect, x[i]);
-        }
+        check2dArray(x, expected);
 
     }
 
@@ -62,29 +52,27 @@ public class RotateMatrixTest {
         x = new int[][]{{1, 2}, {3, 4}};
         expected = new int[][]{{2, 4}, {1, 3}};
         x = underTest.rotateLeft(x);
-        for (int i = 0; i < expected.length; i++) {
-            int[] expect = expected[i];
-            assertArrayEquals(expect, x[i]);
-        }
+        check2dArray(x, expected);
 
         //3x3
         x = new int[][]{{1, 2, 3}, {4,5,6}, {7,8,9}};
         expected = new int[][]{{3,6,9}, {2,5,8}, {1,4,7}};
         x = underTest.rotateLeft(x);
-        for (int i = 0; i < expected.length; i++) {
-            int[] expect = expected[i];
-            assertArrayEquals(expect, x[i]);
-        }
+        check2dArray(x, expected);
 
         //4x4
         x = new int[][]{{1, 2, 3, 4}, {5,6,7,8}, {9,10,11,12},{13,14,15,16}};
         expected = new int[][]{{4,8,12,16}, {3,7,11,15}, {2,6,10,14}, {1,5,9,13}};
         x = underTest.rotateLeft(x);
+        check2dArray(x, expected);
+
+    }
+    
+    private void check2dArray(int[][] result, int[][] expected){
         for (int i = 0; i < expected.length; i++) {
             int[] expect = expected[i];
-            assertArrayEquals(expect, x[i]);
+            assertArrayEquals(expect, result[i]);
         }
     }
-
 
 }
